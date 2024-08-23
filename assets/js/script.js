c1341fbed3b60c4499726988adcc79cac3d3be82
Takafuji Keita <135183240+takahujikeita@users.noreply.github.com>
2024-08-24 00:30:44 +0800

    8/24 ほぼ完成

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$('#slider').slick({
+    autoplay: true,         //自動再生
+    autoplaySpeed: 0,       //自動再生のスピード
+    speed: 7000,            //スライドスピード
+    slidesToShow: 4,        //スライドの表示枚数
+    cssEase: "linear",      //スライドの動きを等速に
+    pauseOnHover: false,    //ホバーしても止まらないように
+    pauseOnFocus: false,    //フォーカスしても止まらないように
+});
