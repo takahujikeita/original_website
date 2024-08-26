cbfd06358acfa7929d6c1a8be41e320e78138e0e
Takafuji Keita <135183240+takahujikeita@users.noreply.github.com>
2024-08-26 15:24:02 +0800

    8/26

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+//Splideの実装
+
 $('#slider').slick({
     autoplay: true,         //自動再生
     autoplaySpeed: 0,       //自動再生のスピード
@@ -6,4 +8,13 @@ $('#slider').slick({
     cssEase: "linear",      //スライドの動きを等速に
     pauseOnHover: false,    //ホバーしても止まらないように
     pauseOnFocus: false,    //フォーカスしても止まらないように
+});
+
+//ハンバーガーメニューの実装
+const ham = document.querySelector('#js-hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
+const nav = document.querySelector('#js-nav'); //js-navの要素を取得し、変数navに格納
+
+ham.addEventListener('click', function () { //ハンバーガーメニューをクリックしたら
+    ham.classList.toggle('active'); // ハンバーガーメニューにactiveクラスを付け外し
+    nav.classList.toggle('active'); // ナビゲーションメニューにactiveクラスを付け外し
 });
